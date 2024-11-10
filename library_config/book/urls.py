c9e1d6d81195cecc044ba1880ac0ddd212912169
maralimjanv <umarolimjonov2009@gmail.com>
2024-11-10 17:51:50 +0500

    true

--- library_config/book/urls.py
+++ library_config/book/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import (
-    BookListAPiViews, BookDetailApiViews, BookCreateApiViews, BookUpdateApiViews, BookDeleteApiViews, AuthorListApiViews, book_list_views)
+from .views import BookListCreateAPIView, BookRetrieveUpdateDestroyAPIView, AuthorRetrieveAPIView
 
 urlpatterns = [
-    path('books/', BookListAPiViews.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailApiViews.as_view(), name='book-detail'),
-    path('books/create/', BookCreateApiViews.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateApiViews.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteApiViews.as_view(), name='book-delete'),
-    path('authors/', AuthorListApiViews.as_view(), name='author-list'),
-    path('books/list/', book_list_views, name='book-list-views')
+    path('books/', BookListCreateAPIView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view(), name='book-detail'),
+    path('authors/<int:pk>/', AuthorRetrieveAPIView.as_view(), name='author-detail'),
 ]
